3c0620965878124ab9db7d43581e12d13069ce25
Danilo Franceschi <danilo@okn.com.br>
2020-02-08 20:07:00 -0300

    Criado o seeder; criado observer de customer

--- www/database/seeds/DatabaseSeeder.php
+++ www/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Enterprises;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = Faker\Factory::create();
+        $count_user = 0;
+
+        while($count_user < 5){
+            $user = User::create([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'password' => $faker->password,
+            ]);
+
+            $empresa = Enterprises::create([
+                'nome' => $faker->name
+            ]);
+
+            $user->Customer()->create([
+                'cpf' => $faker->numberBetween('10000000000', '99999999999'),
+                'nome' => $faker->unique()->safeEmail,
+                'empresa_id' => $empresa->id
+            ]);
+
+            $count_user++;
+        }
     }
 }
